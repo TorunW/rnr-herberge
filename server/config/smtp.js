f112ce94728011d6319600d12b08ea7d72e7e2b3
TorunW <torun.wikstrom@gmail.com>
2021-10-08 20:35:48 +0200

    gitignore2

--- server/config/smtp.js
+++ server/config/smtp.js
@@ -12,20 +12,3 @@ exports.getSmtp = () => {
     },
   };
 };
-
-// exports.getSmtpBooking = () => {
-//   return {
-//     host: 'smtp.ionos.de',
-//     port: 465,
-//     secure: true, // true for 465, false for other ports
-//     logger: true,
-//     debug: true,
-//     auth: {
-//       user: 'buchung@rnrherberge.de ', // generated ethereal user
-//       pass: '???3fragezeichen', // generated ethereal password
-//     },
-//     tls: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// };
