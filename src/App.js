427e7c0dc27f1f95c48527d7fdfb52285682f351
TorunW <torun.wikstrom@gmail.com>
2021-10-08 17:19:38 +0200

    17

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div className={'App' + (loading === false ? ' loading' : '')}>
-      {/* <img src="bg.jpg" className="background-img" /> */}
+      <div className="image-hero-area"></div>
       {templateDisplay}
     </div>
   );
